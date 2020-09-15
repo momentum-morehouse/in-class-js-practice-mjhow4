574cc175275ff5722e124c5eb52601abce2036d1
Marlon Howard <marl04coder@outlook.com>
2020-09-15 00:03:29 -0400

    assignment complete

--- homework.js
+++ homework.js
@@ -5,11 +5,24 @@
 // reassign your variable the value "peach"
 // console log your variable
 
+    let fruit = "banana";
+    console.log(fruit);
+    fruit = "peach";
+    console.log(fruit);
+
 // Problem 2
 // write a function called product that accepts two numbers and 
 // logs the product in the console and then
 // returns the answer when you multiply them both together
 // call that function on the numbers 4 & 689 to test it
+
+function product(a, b) {
+    let multNums = a*b;
+    console.log(multNums);
+    return multNums;
+}
+
+product(4, 689);
 
 // Problem 3
 // write a function called evens that takes an array of numbers, removes 
@@ -17,12 +30,50 @@
 // remember that even numbers have a remainder of 0 when you divide them by 2
 // call your function on this array of numbers [4, 37, 18, 29, 52, 103, 22]
 
+function evens(array) {
+    let newArray = [];
+    let i = 0;
+    while (i < array.length) {
+        if (array[i] % 2 == 0) {
+           newArray.push(array[i]);
+        } else {
+          
+        }
+       i++;
+    } return newArray;
+  }
+
+  evens([4, 37, 18, 29, 52, 103, 22]);
+
 //Problem 4
 // write a function called five that takes a word and prints it in the console
 // five times
 // call your function on the word "pineapple" to test it
-
+function five(word) {
+    i = 0;
+    while (i < 5) {
+        console.log(word);
+        i++
+    }
+}
+five("pineapple");
+       
 //Problem 5
 // write a function called shortWords that takes an array of words and then
 // returns a new array with only the words that are 3 letters long or less
 // call your function on this array ["basin", "pit", "cheese", "bun", "river"]
+
+function shortWords(array) {
+    let newArray = [];
+    let i = 0;
+    while (i < array.length) {
+        if (array[i].length <= 3) {
+           newArray.push(array[i]);
+        } else {
+          
+        }
+       i++;
+    } return newArray;
+  }
+  
+  shortWords(["basin", "pit", "cheese", "bun", "river"]);
